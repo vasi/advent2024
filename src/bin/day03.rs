7672134fc3ab2ed02c429c612da0f5ce0568e145
Dave Vasilevsky <dave@vasilevsky.ca>
2024-12-03 03:43:33 -0500

    day3-2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,21 +1,27 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use regex::RegexBuilder;
 use std::env::args;
 use std::fs::read_to_string;
 
-fn part1(s: &str) -> Result<i64> {
-    let re = RegexBuilder::new(r"mul\( (\d{1,3}) , (\d{1,3}) \)")
+fn calc(s: &str, with_enablement: bool) -> Result<i64> {
+    let re = RegexBuilder::new(r" do\(\)|don\'t\(\)|mul\((\d{1,3}),(\d{1,3})\)")
         .ignore_whitespace(true)
         .build()?;
     let mut ret = 0;
+    let mut enabled = true;
 
-    for (_, caps) in re.captures_iter(s).map(|c| c.extract::<2>()) {
-        let try_parsed: Result<Vec<i64>, _> = caps.iter().map(|c| c.parse()).collect();
-        let parsed = try_parsed?;
-        ret += parsed
-            .into_iter()
-            .reduce(|a, b| a * b)
-            .ok_or(anyhow!("empty"))?;
+    for caps in re.captures_iter(s) {
+        match caps.get(0).unwrap().as_str() {
+            "do()" => enabled = true,
+            "don't()" => enabled = false,
+            _ => {
+                let a1: i64 = caps.get(1).unwrap().as_str().parse()?;
+                let a2: i64 = caps.get(2).unwrap().as_str().parse()?;
+                if enabled || !with_enablement {
+                    ret += a1 * a2;
+                }
+            }
+        }
     }
 
     Ok(ret)
@@ -24,5 +30,6 @@ fn part1(s: &str) -> Result<i64> {
 fn main() {
     let fname = args().nth(1).unwrap();
     let s = read_to_string(fname).unwrap();
-    println!("Part 1: {}", part1(&s).unwrap());
+    println!("Part 1: {}", calc(&s, false).unwrap());
+    println!("Part 2: {}", calc(&s, true).unwrap());
 }
