dda1c833a2e9864b0dee36fcaa655b2b5de65044
Dave Vasilevsky <dave@vasilevsky.ca>
2024-12-07 01:27:56 -0500

    day07-2

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -7,16 +7,28 @@ struct Equation {
     operands: Vec<i64>,
 }
 
+fn concat(a: i64, b: i64) -> i64 {
+    let bdigits = b.checked_ilog10().unwrap_or(0) + 1;
+    a * 10_i64.pow(bdigits) + b
+}
+
 impl Equation {
-    fn soluble_inner(&self, idx: usize, val: i64) -> bool {
+    fn soluble_inner(&self, allow_concat: bool, idx: usize, val: i64) -> bool {
         match self.operands.get(idx) {
             None => self.result == val,
-            Some(n) => self.soluble_inner(idx + 1, val + n) || self.soluble_inner(idx + 1, val * n),
+            Some(n) => {
+                let mut nvs = vec![val + n, val * n];
+                if allow_concat {
+                    nvs.push(concat(val, *n))
+                }
+                nvs.iter()
+                    .any(|nv| self.soluble_inner(allow_concat, idx + 1, *nv))
+            }
         }
     }
 
-    fn soluble(&self) -> bool {
-        self.soluble_inner(1, *self.operands.get(0).unwrap())
+    fn soluble(&self, allow_concat: bool) -> bool {
+        self.soluble_inner(allow_concat, 1, *self.operands.get(0).unwrap())
     }
 }
 
@@ -38,15 +50,20 @@ fn parse(fname: &str) -> Vec<Equation> {
         .collect()
 }
 
+fn solve(equations: &Vec<Equation>, allow_concat: bool) -> i64 {
+    let mut tot = 0;
+    for eq in equations {
+        if eq.soluble(allow_concat) {
+            tot += eq.result;
+        }
+    }
+    tot
+}
+
 fn main() {
     let fname = args().nth(1).unwrap();
     let equations = parse(&fname);
 
-    let mut tot = 0;
-    for eq in equations {
-        if eq.soluble() {
-            tot += eq.result;
-        }
-    }
-    println!("Part 1: {}", tot);
+    println!("Part 1: {}", solve(&equations, false));
+    println!("Part 2: {}", solve(&equations, true));
 }
