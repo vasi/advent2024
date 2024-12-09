aacb7fb9db1cc460f00731a5a3ca4522fe30c970
Dave Vasilevsky <dave@vasilevsky.ca>
2024-12-09 14:27:42 -0500

    day09-2

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -2,22 +2,51 @@ use std::env::args;
 use std::fmt;
 use std::fs;
 
+type Bitmap = Vec<Option<i64>>;
+
+#[derive(Clone)]
+struct Gap {
+    pos: usize,
+    size: usize,
+}
+
+#[derive(Clone)]
+struct File {
+    #[allow(unused)]
+    id: i64,
+    pos: usize,
+    size: usize,
+}
+
 #[derive(Clone)]
 struct Disk {
-    bitmap: Vec<Option<i64>>,
+    bitmap: Bitmap,
+    files: Vec<File>,
+    gaps: Vec<Gap>,
 }
 
 impl Disk {
     fn parse(fname: &str) -> Self {
         let contents = fs::read_to_string(fname).unwrap();
-        let mut bitmap = Vec::new();
+        let mut bitmap = Bitmap::new();
+        let mut files = Vec::new();
+        let mut gaps = Vec::new();
         let mut next_id = 0;
         let mut next_is_block = true;
         for c in contents.chars() {
             let size = c.to_digit(10).unwrap();
             let n = if next_is_block {
+                files.push(File {
+                    id: next_id,
+                    pos: bitmap.len(),
+                    size: size as usize,
+                });
                 Some(next_id)
             } else {
+                gaps.push(Gap {
+                    pos: bitmap.len(),
+                    size: size as usize,
+                });
                 next_id += 1;
                 None
             };
@@ -26,7 +55,11 @@ impl Disk {
             }
             next_is_block = !next_is_block;
         }
-        Self { bitmap }
+        Self {
+            bitmap,
+            files,
+            gaps,
+        }
     }
 
     fn compact1(&mut self) {
@@ -43,6 +76,26 @@ impl Disk {
         }
     }
 
+    fn move_file_in_bitmap(bitmap: &mut Bitmap, file: &File, gap: &mut Gap) {
+        for i in 0..file.size {
+            bitmap.swap(gap.pos + i, file.pos + i);
+        }
+        gap.pos += file.size;
+        gap.size -= file.size;
+    }
+
+    fn compact2(&mut self) {
+        for file in self.files.iter().rev() {
+            if let Some(gap) = self
+                .gaps
+                .iter_mut()
+                .find(|g| g.pos < file.pos && g.size >= file.size)
+            {
+                Self::move_file_in_bitmap(&mut self.bitmap, file, gap);
+            }
+        }
+    }
+
     fn score1(&self) -> i64 {
         let mut tot = 0;
         for (i, v) in self.bitmap.iter().enumerate() {
@@ -55,6 +108,11 @@ impl Disk {
 
     fn part1(&mut self) -> i64 {
         self.compact1();
+        self.score1()
+    }
+
+    fn part2(&mut self) -> i64 {
+        self.compact2();
         self.score1()
     }
 }
@@ -78,5 +136,6 @@ impl fmt::Display for Disk {
 fn main() {
     let fname = args().nth(1).unwrap();
     let disk = Disk::parse(&fname);
-    println!("{}", disk.clone().part1());
+    println!("Part 1: {}", disk.clone().part1());
+    println!("Part 2: {}", disk.clone().part2());
 }
