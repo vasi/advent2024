cacb0114b2e85fa66c7597bbe1f9e1dd038374a3
Dave Vasilevsky <dave@vasilevsky.ca>
2024-12-19 03:34:56 -0500

    day19-2

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -7,7 +7,7 @@ struct Onsen {
     designs: Vec<String>,
 }
 
-type Cache = HashMap<String, bool>;
+type Cache = HashMap<String, usize>;
 
 impl Onsen {
     fn parse(fname: &str) -> Self {
@@ -29,36 +29,36 @@ impl Onsen {
         Self { patterns, designs }
     }
 
-    fn possible(patterns: &Vec<String>, cache: &mut Cache, design: &str) -> bool {
+    fn possible(patterns: &Vec<String>, cache: &mut Cache, design: &str) -> usize {
         if let Some(v) = cache.get(design) {
             return *v;
         }
 
-        let mut found = false;
+        let mut total = 0;
         for pat in patterns {
             if let Some(suffix) = design.strip_prefix(pat) {
-                if Self::possible(patterns, cache, suffix) {
-                    found = true;
-                    break;
-                }
+                let count = Self::possible(patterns, cache, suffix);
+                total += count;
             }
         }
-        cache.insert(design.to_owned(), found);
-        found
+        cache.insert(design.to_owned(), total);
+        total
     }
 
-    fn part1(&self) -> usize {
+    fn counts(&self) -> Vec<usize> {
         let mut cache = Cache::new();
-        cache.insert("".to_owned(), true);
+        cache.insert("".to_owned(), 1);
         self.designs
             .iter()
-            .filter(|d| Self::possible(&self.patterns, &mut cache, d))
-            .count()
+            .map(|d| Self::possible(&self.patterns, &mut cache, d))
+            .collect()
     }
 }
 
 fn main() {
     let fname = args().nth(1).unwrap();
     let onsen = Onsen::parse(&fname);
-    println!("{}", onsen.part1());
+    let counts = onsen.counts();
+    println!("Part 1: {}", counts.iter().filter(|c| **c > 0).count());
+    println!("Part 2: {}", counts.iter().copied().sum::<usize>());
 }
