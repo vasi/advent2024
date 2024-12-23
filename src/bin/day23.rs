48dff49bf29dae8a6b97fbe218fdaac028f1308d
Dave Vasilevsky <dave@vasilevsky.ca>
2024-12-23 02:30:22 -0500

    day23-2

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -40,11 +40,7 @@ impl Network {
                 if a != b {
                     let bconn = self.connections.get(b).unwrap();
                     for c in aconn.intersection(bconn) {
-                        let triplet = vec![a, b, c]
-                            .iter()
-                            .sorted()
-                            .map(|x| (*x).to_owned())
-                            .collect();
+                        let triplet = vec![a, b, c].iter().cloned().cloned().sorted().collect();
                         ret.insert(triplet);
                     }
                 }
@@ -59,10 +55,51 @@ impl Network {
             .filter(|tr| tr.iter().any(|s| s.starts_with("t")))
             .count()
     }
+
+    fn bk_simple(
+        &self,
+        r: HashSet<Computer>,
+        mut p: HashSet<Computer>,
+        mut x: HashSet<Computer>,
+        m: &mut Vec<HashSet<Computer>>,
+    ) {
+        if p.is_empty() && x.is_empty() {
+            m.push(r.clone());
+        }
+        for v in p.clone() {
+            let nv = self.connections.get(&v).unwrap();
+            self.bk_simple(
+                r.iter().chain(Some(&v)).cloned().collect(),
+                p.intersection(nv).cloned().collect(),
+                x.intersection(nv).cloned().collect(),
+                m,
+            );
+            p.remove(&v);
+            x.insert(v);
+        }
+    }
+
+    fn maximal_cliques(&self) -> Vec<HashSet<Computer>> {
+        let mut m = Vec::new();
+        self.bk_simple(
+            HashSet::new(),
+            self.connections.keys().map(|s| s.to_owned()).collect(),
+            HashSet::new(),
+            &mut m,
+        );
+        m
+    }
+
+    fn part2(&self) -> String {
+        let cliques = self.maximal_cliques();
+        let max = cliques.iter().max_by_key(|c| c.len()).unwrap();
+        max.iter().sorted().join(",")
+    }
 }
 
 fn main() {
     let fname = args().nth(1).unwrap();
     let network = Network::parse(&fname);
     println!("Part 1: {}", network.part1());
+    println!("Part 2: {}", network.part2());
 }
